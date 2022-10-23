7a7179809738f4c08d74888a63f5e94433e13bd3
graduate <gr120210400@cspro.sogang.ac.kr>
2022-10-22 21:32:29 -0700

    Start MPI study

---
+++ self_study/mpi/mpi_hello.c
@@ -0,0 +1,13 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int rank, num, i;
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    MPI_Comm_size (MPI_COMM_WORLD, &num);
+    printf("Hello from process %i of %i \n", rank, num);
+    MPI_Finalize ();
+    return 0;
+}
