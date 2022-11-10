7d0ee1fe7213b31edbe98d4cdd16b687ee2bb924
yh-raphael <yhcho.raphael@gmail.com>
2022-11-10 12:15:56 +0000

    Now on making psum

--- homework2/README.md
+++ homework2/README.md
@@ -1,7 +1,11 @@
 # Problem 1
 
+## Compilation
 Please, go to the directory ./p1 and input the command "$ make".
 
+## Execution
+usage: $ ./palindrome <# of threads> <input_file> <output_file>
+e.g. : $ ./palindrome 4 words.txt output.txt
 
 # Problem 2
 
--- homework2/p1/palindrome.c
+++ homework2/p1/palindrome.c
@@ -2,16 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <omp.h>
+#include <sys/time.h>
 
-#define MAX_NAME_LEN    32
+#define MAX_LINE        30000
+#define MAX_NAME_LEN    64
+#define MAX_STR_LEN     64
+
+FILE *ifp, *ofp;
+char word_pool[MAX_LINE][MAX_STR_LEN];
+int count;
+
+char rev[MAX_STR_LEN];
+char arr[MAX_STR_LEN];
 
 void Hello (void);   /* 스레드 함수 */
 
+/* 인풋 파일 기반 word pool 생성 함수 */
+void create_pool (void)
+{
+    int i = 0;
+
+    while (fscanf (ifp, "%s\n", word_pool[i++]) != EOF);
+
+    count = i;
+}
+
+void reverse_string (void)
+{
+    strcpy(rev, arr);    // arr 배열을 rev 배열에 복사
+    for (int i = 0; i < strlen(rev) / 2; i++) {        // rev 배열의 반만 돌아도 된다
+        // swap
+        char temp = rev[i];
+        rev[i] = rev[strlen(rev) - 1 - i];    // 배열이 0부터 시작하므로 -1을 해 줌
+        rev[strlen(rev) - 1 - i] = temp;
+    }
+}
+
+int is_palindrome (void)
+{
+    reverse_string();
+    if (!strcmp (arr, rev)) {                       // reverse랑 같으면 palindrome.
+        fprintf (ofp, "%s\n", arr);
+        return 1;
+    }
+    else {
+        for (int i = 0; i < count; i++) {
+            if (!strcmp (rev, word_pool[i])) {      // reverse가 pool에 있으면 palindrome.
+                fprintf (ofp, "%s\n", arr);
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
 int main (int argc, char* argv[])
 {
-    FILE *ifp, *ofp;
     char input_file[MAX_NAME_LEN];
     char output_file[MAX_NAME_LEN];
+    //struct timeval t;
+    double start, finish, elapsed;
+    //int re;
 
     if (argc != 4) {
         fprintf (stderr, "usage: a.out <# of threads> <input_file> <output_file>\n");
@@ -35,12 +86,49 @@ int main (int argc, char* argv[])
     }
 
     /* 시작 */
-    fprintf(ofp, "hello\n");
+    create_pool();
+
+    /* 병렬화 부분 */
+    // gettimeofday(&t, NULL);
+    // start = t.tv_sec + t.tv_usec / 1000000.0;
+    start = omp_get_wtime ();
+
+#   pragma omp parallel for
+    for (int i = 0; i < count; i++) {
+        strcpy (arr, word_pool[i]);
+        //is_palindrome();
+        //printf("re: %d \n", re);
+        #pragma omp critical
+        reverse_string();
+
+        if (!strcmp (arr, rev)) {                       // reverse랑 같으면 palindrome.
+            #pragma omp critical
+            fprintf (ofp, "%s\n", arr);
+            //return 1;
+        }
+        else {
+            for (int i = 0; i < count; i++) {
+                if (!strcmp (rev, word_pool[i])) {      // reverse가 pool에 있으면 palindrome.
+                    #pragma omp critical
+                    fprintf (ofp, "%s\n", arr);
+                    //return 1;
+                }
+            }
+        }
+        //return 0;
+    }
+
+    // gettimeofday(&t, NULL);
+    // finish = t.tv_sec + t.tv_usec / 1000000.0;
+    finish = omp_get_wtime ();
+
+    elapsed = finish - start;
+    printf("The code to be timed took %e seconds\n", elapsed);
 
 
 
-#   pragma omp parallel num_threads(thread_count)
-    Hello();
+//#   pragma omp parallel num_threads(thread_count)
+    //Hello();
 
 
     fclose (ifp);
@@ -48,10 +136,11 @@ int main (int argc, char* argv[])
     return 0;
 } /* main */
 
-void Hello (void)
-{
-    int my_rank = omp_get_thread_num();
-    int thread_count = omp_get_num_threads();
+// void Hello (void)
+// {
+//     int my_rank = omp_get_thread_num();
+//     int thread_count = omp_get_num_threads();
 
-    printf("Hello from thread %d of %d \n", my_rank, thread_count);
-} /* Hello */
+//     printf("Hello from thread %d of %d \n", my_rank, thread_count);
+// } /* Hello */
+
